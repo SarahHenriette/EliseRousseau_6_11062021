d19a7263102299514b7d9d95ff194e27f3472202
elise <rousseaujumelle@gmail.com>
2021-07-03 16:09:43 +0200

    integration card photograph homepage

--- js/Photographers.js
+++ js/Photographers.js
@@ -1,4 +1,3 @@
-const main = document.querySelector(".photographsList")
 let json = "../data.json"
 
 fetch(json).then((res)=>{ 
@@ -8,27 +7,71 @@ fetch(json).then((res)=>{
         console.log(res.status)
     }
 }).then(data => {
-      data.photographers.forEach(photographe => {
-        console.log(photographe)
-        let photograph = document.createElement('a')
-        let photographHeader = document.createElement('header') 
-        let photographBody = document.createElement('body') 
-        let photographFooter = document.createElement('footer') 
-
-
-        photograph.setAttribute("href", "#")
-        photograph.classList.add("photographsList-photograph")
-        photographHeader.classList.add("photographsList-photograph_header")
-        photographBody.classList.add("photographsList-photograph_body")
-        photographFooter.classList.add("photographsList-photograph_footer")
-
-
-        main.appendChild(photograph)
-        photograph.appendChild(photographHeader)
-        photograph.appendChild(photographBody)
-        photograph.appendChild(photographFooter)
+      data.photographers.forEach(dataPhotograph => {
+        createCard(dataPhotograph)
       });
   
 }).catch((err) => {
     console.log(err)
 })
+
+function createCard (dataPhotograph){
+    //récuperation du conteneur des cards photographes
+    const main = document.querySelector(".photographsList")
+
+    //créations des éléments pour créer les cards des photographes
+    let photograph = document.createElement('a')
+    let photographHeader = document.createElement('header') 
+    let photographContainImage = document.createElement('div')
+    let photographImage = document.createElement('img')
+    let photographName = document.createElement('h2')
+    let photographBody = document.createElement('body') 
+    let photographCity= document.createElement('p') 
+    let photographSlogan= document.createElement('p') 
+    let photographPrice= document.createElement('p') 
+    let photographFooter = document.createElement('footer') 
+
+    //attributions des class aux éléments
+    photograph.classList.add("photograph")
+    photographHeader.classList.add("photograph-header")
+    photographContainImage.classList.add("photograph-header_image")
+    photographName.classList.add("photograph-header_title")
+    photographBody.classList.add("photograph-body")
+    photographCity.classList.add("photograph-body_city")
+    photographSlogan.classList.add("photograph-body_job")
+    photographPrice.classList.add("photograph-body_price")
+    photographFooter.classList.add("photograph-footer")
+
+    //attributiond des attributs
+    photograph.setAttribute("href", "#")
+    photographImage.setAttribute("src", "../img/PhotographIDPhoto/" + dataPhotograph.portrait)
+
+    
+    //integration text dans les elements
+    photographName.innerHTML = dataPhotograph.name
+    photographCity.innerHTML = dataPhotograph.city + ", " + dataPhotograph.country
+    photographSlogan.innerHTML = dataPhotograph.tagline
+    photographPrice.innerHTML = dataPhotograph.price + "E/jour" 
+
+    //integration des tags dans le footer
+    dataPhotograph.tags.forEach(tag => {
+        console.log(tag)
+        let t = document.createElement("a")
+        t.setAttribute("href", "#")
+        t.classList.add("btnTags")
+        t.innerHTML = "#" + tag
+        photographFooter.appendChild(t)
+    });
+
+    //rattachement des éléments au DOM 
+    main.appendChild(photograph)
+    photograph.appendChild(photographHeader)
+    photographHeader.appendChild(photographContainImage)
+    photographContainImage.appendChild(photographImage)
+    photographHeader.appendChild(photographName)
+    photograph.appendChild(photographBody)
+    photographBody.appendChild(photographCity)
+    photographBody.appendChild(photographSlogan)
+    photographBody.appendChild(photographPrice)
+    photograph.appendChild(photographFooter)
+}
