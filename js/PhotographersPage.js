9997e8d3939e36925591374d7f0643f6801a3f6b
elise <rousseaujumelle@gmail.com>
2021-07-08 17:27:10 +0200

    formulaire page photographe

--- js/PhotographersPage.js
+++ js/PhotographersPage.js
@@ -1,10 +1,13 @@
 //récuperation de l'id du photograph
 let paramSearch = window.location.search
 let id = paramSearch.replace("?id=", "")
+let namePhotographe 
 
+
+//creer la card header de la page photographe
 class CreateCardHeader {
     constructor (data) {
-        console.log(data)
+        // console.log(data)
         this.dataPhotograph = data
         this.firstName = document.querySelector('.photographerHeader-text_name')
         this.city = document.querySelector('.photographerHeader-text_city')
@@ -39,24 +42,21 @@ class CreateCardHeader {
     } 
 }
 
+//créer la card media de la page photographe
 class CreateMediaCard {
     constructor(data, namePhotographe){
         this.dataMedia = data
-        console.log(this.dataMedia)
-
         this.namePhotographe = namePhotographe
         this.main = document.querySelector("#mediasList")
         this.media = document.createElement('article')
         this.mediaHeader = document.createElement('header')
         this.mediaHeaderLinkImage = document.createElement('a')
         if(this.dataMedia.image){
-            console.log("image")
             this.mediaHeaderImage = document.createElement('img')
             this.mediaHeaderImage.setAttribute("src", "../img/"+ this.namePhotographe +"/"+ this.dataMedia.image)
             this.mediaHeaderLinkImage.appendChild(this.mediaHeaderImage)
 
         }else if (this.dataMedia.video){
-            console.log("video")
             this.mediaHeaderVideo = document.createElement('video')
             this.mediaHeaderVideoSrc = document.createElement('source')
 
@@ -67,8 +67,6 @@ class CreateMediaCard {
             this.mediaHeaderLinkImage.appendChild(this.mediaHeaderVideo)
             this.mediaHeaderVideo.appendChild(this.mediaHeaderVideoSrc)
             this.mediaHeaderVideo.setAttribute("src", "../img/"+ this.namePhotographe +"/"+ this.dataMedia.video)
-
-
         }
         this.mediaFooter = document.createElement('footer')
         this.mediaName = document.createElement('p')
@@ -92,8 +90,6 @@ class CreateMediaCard {
 
     attributionAttribute(){
         this.mediaHeaderLinkImage.setAttribute("href", "#")
-        // this.mediaHeaderImage.setAttribute("src", "../img/"+ this.namePhotographe +"/"+ this.dataMedia.image)
-        // this.mediaHeaderVideo.setAttribute("controls", "")
     }
 
     integrationTextElement(){
@@ -106,11 +102,102 @@ class CreateMediaCard {
         this.media.appendChild(this.mediaHeader)
         this.media.appendChild(this.mediaFooter)
         this.mediaHeader.appendChild(this.mediaHeaderLinkImage)
-        // this.mediaHeaderLinkImage.appendChild(this.mediaHeaderImage)
         this.mediaFooter.appendChild(this.mediaName)
         this.mediaFooter.appendChild(this.mediaNumberLike)
         this.mediaFooter.appendChild(this.mediaButtonLike)
     }
+}
+
+class Formulaire {
+    constructor (data) {
+        this.btnContactMe = document.getElementById("btnContactMe")
+        this.formulaire = document.getElementById("formulaire")
+        this.formulaireNamePhotograph = document.getElementById("formulaire-namePhotographe")
+        this.data = data
+        this.firstname= document.querySelector("#prenom")
+        this.lastname= document.querySelector("#nom")
+        this.email= document.querySelector("#email")
+        this.message= document.querySelector("#message")
+        this.regexEmail=  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        this.regexMessage = /^.{10,35}$/
+        this.regexMin = /^.{2,35}$/
+        this.formulaireName()
+        this.addEvent(this.firstname, this.regexMin, "Veuillez entrer 2 caractères ou plus pour le champ du prénom.")
+        this.addEvent(this.lastname, this.regexMin, "Veuillez entrer 2 caractères ou plus pour le champ du prénom.")
+        this.addEvent(this.email, this.regexEmail, "Veuillez entrer une adresse mail valide.")
+        this.addEvent(this.message, this.regexMessage, "Veuillez entrer 10 caractères ou plus pour le champ du prénom.")
+        this.submitForm()
+        this.btnClose= document.querySelector(".formulaire-close")
+        this.closeForm()
+        this.openForm()
+    }
+
+    formulaireName() {
+        this.formulaireNamePhotograph.innerHTML = this.data
+    }
+
+    submitForm(){
+        this.formulaire.addEventListener("submit", (e)=> {
+            e.preventDefault()
+            if( this.regexMin.test(this.firstname.value) &&
+                this.regexMin.test(this.lastname.value) &&
+                this.regexEmail.test(this.email.value) &&
+                this.regexMessage.test(this.message.value)){
+                    let data = {
+                        "prenom": this.firstname.value,
+                        "nom":  this.lastname.value,
+                        "email": this.email.value,
+                        "message": this.message.value
+                    }
+                    //affichage des valeurs du form
+                    console.log(data)
+
+                    //reset le form
+                    e.target.reset()
+                    const textControl = document.querySelectorAll(".text-control")
+                    textControl.forEach(element => {
+                        element.classList.remove("success")
+                    });
+        
+                }
+            })
+    }
+
+    closeForm() {
+        this.btnClose.addEventListener("click", ()=>{
+            this.formulaire.style.display= "none"
+        })
+    }
+
+    openForm() {
+        this.btnContactMe.addEventListener("click", ()=>{
+            this.formulaire.style.display= "block"
+        })
+    }
+
+    addEvent(first, regex, textMsgErreur){
+        first.addEventListener("keyup", function(){
+            let msgErreur = first.parentNode.childNodes[5]
+            if(regex.test(first.value)) {
+                success(first, msgErreur)
+            }else {
+                error(first, textMsgErreur, msgErreur )
+            }
+        })
+        function success(element, smallError) {
+            element.classList.add("success")
+            element.classList.remove("error") 
+            smallError.style.display="none" //supprime le msg d'erreur 
+        }
+
+        function error(element, messageError, smallError) {
+            element.classList.add("error")
+            element.classList.remove("success")
+            smallError.style.display="block" //affiche le msg d'erreur 
+            smallError.innerHTML= messageError
+        }
+    }
+
 }
 
 //recup des donnes json
@@ -119,25 +206,20 @@ fetch("../data.json").then((res)=>{
         return res.json()
     }
 }).then(datas => {
-    let namePhotographe 
     //recup infos photograph
     for(let data of datas.photographers){
         if(data.id == parseInt(id)) {
             new CreateCardHeader(data)
             namePhotographe = data.name
+            new Formulaire(data.name)
+
         }
     }
     //recup media photograph
     for(let data of datas.media){
         if(data.photographerId == parseInt(id)) {
-            
-            // namePhotographe =  namePhotographe.split("-")[0].split(" ")[0]
-                namePhotographe =  namePhotographe.replace("-", "").split(" ")[0]
-   
-            console.log(namePhotographe)
+            namePhotographe =  namePhotographe.replace("-", "").split(" ")[0]
             new CreateMediaCard(data, namePhotographe)
-            // console.log(namePhotographe.split("-")[0].split(" ")[0])
-            // mediaPhotograph.push(data)
         }
     }
 }).catch((err) => {
---
+++ js/formulaire.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById("#btnContactMe")
+let formulaire = document.getElementById("#formulaire")
+
+
+btn.addEventListener("click", ()=>{
+    formulaire.style.display= "flex"
+})
